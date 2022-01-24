70f59f9ef47bd5487f875c3fa2ba7016a8607cd1
Maicomgit <maicom@etica-ti.com.br>
2022-01-24 08:09:52 -0300

    novos arquivos

---
+++ css/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    // $('#divnova').before('antes')
+    // $('#divnova').prepend('escreve dentro em sempre em cima')
+    // $('#divnova').append('<span id="teste">sempre em baixo do que ja tem ')
+    // $('#divnova').after('depois ')
+    // $('#divnova').remove()
+    
+    // $('#divneta').parent().text('oi')
+    $('#divpai').find('#divneta').length
+})
+
+
+    
